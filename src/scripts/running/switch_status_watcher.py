43a9fdb5911402bfe0489ad2298e0918dd0642cb
gwendalda <gwendalda@gmail.com>
2024-03-22 18:43:22 -0400

    minor-changes

--- src/scripts/running/switch_status_watcher.py
+++ src/scripts/running/switch_status_watcher.py
@@ -18,6 +18,8 @@ def get_all_devices() -> dict:
     for device in data:
         if device["ip"] == "":
             continue
+        if device["err"]:
+            continue
         if "1" not in device["dps"]["dps"]:
             device_instantiation = tinytuya.BulbDevice(device["id"], device["ip"], device["key"])
             device_instantiation.set_version(3.3)
