767fddb0108ac1cfc5d56ea2ec5cdf5678057258
gwendalda <gwendalda@gmail.com>
2024-04-21 19:26:08 -0400

    added support for changing scenes with brightness controller

--- src/classes/animations/Scene.py
+++ src/classes/animations/Scene.py
@@ -46,11 +46,11 @@ class Rotation:
         self.transition_mode = "02"
 
     def set_color(self, red:int, green:int, blue:int):
-        if red < 0 or red > 1000:
+        if red < 0 or red > 2000:
             raise ValueError("Red value must be between 0 and 1000")
-        if green < 0 or green > 1000:
+        if green < 0 or green > 2000:
             raise ValueError("Green value must be between 0 and 1000")
-        if blue < 0 or blue > 1000:
+        if blue < 0 or blue > 2000:
             raise ValueError("Blue value must be between 0 and 1000")
         # all the hex string must be 4 characters long with leading 0s
         self.red = str(format_to_hex(red))
@@ -59,11 +59,10 @@ class Rotation:
         return self
     
     def set_white(self, white:int):
-        if self.red != "000" or self.green != "000" or self.blue != "000":
-            raise ValueError("White value can only be set if the color is white")
-        if white < 0 or white > 1000:
+        self.set_color(0, 0, 0 )
+        if white < 0 or white > 1000 :
             raise ValueError("White value must be between 0 and 1000")
-        self.white_string = str(format_to_hex(white))
+        self.white_string = str(format_to_hex(white)) + str(format_to_hex(white))
         return self
 
     def set_duration(self, duration_1:int, duration_2:int):
@@ -104,26 +103,44 @@ def get_all_devices():
     return devices
 
 if __name__ == "__main__":
+    print(str(format_to_hex(1000)))
     # an interactive way to create scenes
     scene = Scene(input("Enter the name of the scene : "))
     while True:
         rotation = Rotation()
+        white_bool = "j"
+        while white_bool not in ["y", "n"] :
+            white_bool = input("Is the color white ? (Y/N) : ").lower()
+            if white_bool not in ["y", "n"]:
+                print("Invalid input. Please enter Y or N.")
+        if white_bool == "y":
+            while True:
+                try:
+                    white = int(input("White (0-1000): "))
+                    if 0 <= white <= 1000:
+                        rotation.set_white(white)
+                        break
+                    else:
+                        print("Invalid white value. Please enter a value between 0 and 1000.")
+                except ValueError:
+                    print("Invalid input. Please enter numeric values.")
+        else:
+                
+            # Validate and set color
+            while True:
+                try:
+                    hue = int(input("Hue (0-360): "))
+                    saturation = int(input("Saturation (0-1000): "))
+                    brightness = int(input("Brightness (0-1000): "))
+                    if 0 <= hue <= 360 and 0 <= saturation <= 1000 and 0 <= brightness <= 1000:
+                        rotation.set_color(hue, saturation, brightness)
+                        break
+                    else:
+                        print("Invalid color values. Please enter values between 0 and 1000.")
+                except ValueError:
+                    print("Invalid input. Please enter numeric values.")
 
-        # Validate and set color
-        while True:
-            try:
-                red = int(input("Red (0-1000): "))
-                green = int(input("Green (0-1000): "))
-                blue = int(input("Blue (0-1000): "))
-                if 0 <= red <= 1000 and 0 <= green <= 1000 and 0 <= blue <= 1000:
-                    rotation.set_color(red, green, blue)
-                    break
-                else:
-                    print("Invalid color values. Please enter values between 0 and 1000.")
-            except ValueError:
-                print("Invalid input. Please enter numeric values.")
-
-        # Validate and set duration
+            # Validate and set duration
         while True:
             try:
                 duration_1 = int(input("Duration 1 (0-100): "))
--- src/scripts/running/switch_status_watcher.py
+++ src/scripts/running/switch_status_watcher.py
@@ -4,7 +4,8 @@ import json
 import pprint
 import os
 
-SNAPSHOT_PATH = "/home/gwendal/wifi-lights/snapshot.json"
+SNAPSHOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))+ "/snapshot.json"
+SCENES_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))+ "/scenes.json"
 
 def get_all_devices() -> dict:
 
@@ -29,7 +30,28 @@ def get_all_devices() -> dict:
             devices["switches"].append(device_instantiation)
         
     return devices
-     
+
+
+def change_scene(brightness_level, scenes: dict, bulbs: list):
+    print(brightness_level)
+    for bulb in bulbs:
+        bulb.set_mode('scene')
+        bulb.set_value(25, scenes[brightness_level], nowait=True)     
+
+
+def simple_brightness(brightness_level):
+    if brightness_level < 50:
+        return "1"
+    if brightness_level < 90:
+        return "2"
+    if brightness_level < 135:
+        return "3"
+    if brightness_level < 190:
+        return "4"
+    if brightness_level < 235:
+        return "5"
+    return "6"
+
 def main():
     try:
         devices = get_all_devices()
@@ -44,10 +66,15 @@ def main():
         switch.set_version(3.3)
     current_on_off_status = None
     current_brighness_status = None
+    with open(SCENES_PATH) as f:
+                scenes = json.load(f)
     while True:
         status = switch.status()
+        if 'dps' not in status.keys():
+            continue
         for bulb in devices["bulbs"]:
             time0 = time.time()
+
             if time.time() - time0 > 5:
                 print("Timeout")
                 tinytuya.scan()
@@ -65,14 +92,11 @@ def main():
             #print(devices["bulbs"][0].status()["dps"]['20'])
             for bulb in devices["bulbs"]:
                 bulb.turn_on(nowait=True)
-        if status['dps']['2'] != current_brighness_status and current_on_off_status == True:
+        if status['dps']['2'] != current_brighness_status:
             print("Brightness has changed")
             current_brighness_status = status['dps']['2']
-            # Depending on the current brightness we 
-
-            for bulb in devices["bulbs"]:
-                bulb.set_brightness_percentage(current_brighness_status, nowait=True)
-
+            change_scene(simple_brightness(status['dps']['2']), scenes, devices["bulbs"])
 
 if __name__ == "__main__":
     main()
+    
