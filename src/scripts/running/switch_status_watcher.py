f5730c799d7de182329d575598bce11610607b98
Gwendal Delisle Arnold <gwendalda@gmail.com>
2024-06-28 01:20:45 -0400

    fixed keys problem

--- src/scripts/running/switch_status_watcher.py
+++ src/scripts/running/switch_status_watcher.py
@@ -4,8 +4,11 @@ import json
 import pprint
 import os
 
-SNAPSHOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))+ "/snapshot.json"
-SCENES_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))+ "/scenes.json"
+SNAPSHOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.abspath(os.path.dirname(__file__))))) + "/snapshot.json"
+SCENES_PATH = os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.abspath(os.path.dirname(__file__))))) + "/scenes.json"
+
 
 def get_all_devices() -> dict:
 
@@ -18,19 +21,24 @@ def get_all_devices() -> dict:
     for device in data:
         try:
             if device["ip"] == "":
+                print("No ip address for device")
                 continue
             if device["err"]:
+                print("Error in device")
                 continue
-            
             if "1" not in device["dps"]["dps"]:
-                device_instantiation = tinytuya.BulbDevice(device["id"], device["ip"], device["key"])
+                device_instantiation = tinytuya.BulbDevice(
+                    device["id"], device["ip"], device["key"])
                 device_instantiation.set_version(3.3)
                 devices["bulbs"].append(device_instantiation)
+
             if "1" in device["dps"]['dps']:
-                device_instantiation = tinytuya.OutletDevice(device["id"], device["ip"], device["key"])
+                device_instantiation = tinytuya.OutletDevice(
+                    device["id"], device["ip"], device["key"])
                 device_instantiation.set_version(3.3)
                 devices["switches"].append(device_instantiation)
         except KeyError as e:
+            print(e)
             pass
     return devices
 
@@ -39,7 +47,7 @@ def change_scene(brightness_level, scenes: dict, bulbs: list):
     print(brightness_level)
     for bulb in bulbs:
         bulb.set_mode('scene')
-        bulb.set_value(25, scenes[brightness_level], nowait=True)     
+        bulb.set_value(25, scenes[brightness_level], nowait=True)
 
 
 def simple_brightness(brightness_level):
@@ -54,6 +62,7 @@ def simple_brightness(brightness_level):
     if brightness_level < 235:
         return "5"
     return "6"
+
 
 def main():
     try:
@@ -70,36 +79,38 @@ def main():
     current_on_off_status = None
     current_brighness_status = None
     with open(SCENES_PATH) as f:
-                scenes = json.load(f)
+        scenes = json.load(f)
     while True:
         status = switch.status()
-        if 'dps' not in status.keys():
+        if 'dps' not in status.keys() and "1" not in status["dps"].keys():
             continue
-        for bulb in devices["bulbs"]:
-            time0 = time.time()
-
-            if time.time() - time0 > 5:
-                print("Timeout")
-                tinytuya.scan()
-                devices = get_all_devices()
-                switch = devices["switches"][0]
-                switch.set_version(3.3)   
-        if status["dps"]['1'] == False and devices["bulbs"][0].status()["dps"]['20'] == True and current_on_off_status != False:
+        time0 = time.time()
+        if status["dps"]['1'] == False and current_on_off_status != False:
             print("Switch is off")
             current_on_off_status = False
             for bulb in devices["bulbs"]:
-                bulb.turn_off(nowait=True)
-        if status["dps"]['1'] == True and devices["bulbs"][0].status()["dps"]['20'] == False and current_on_off_status != True:
+                print(bulb)
+                bulb.turn_off()
+        if status["dps"]['1'] == True and current_on_off_status != True:
             print("Switch is on")
             current_on_off_status = True
-            #print(devices["bulbs"][0].status()["dps"]['20'])
+            # print(devices["bulbs"][0].status()["dps"]['20'])
             for bulb in devices["bulbs"]:
-                bulb.turn_on(nowait=True)
+                print(bulb)
+                print(bulb.turn_on())
+
         if status['dps']['2'] != current_brighness_status:
             print("Brightness has changed")
             current_brighness_status = status['dps']['2']
-            change_scene(simple_brightness(status['dps']['2']), scenes, devices["bulbs"])
+            change_scene(simple_brightness(
+                status['dps']['2']), scenes, devices["bulbs"])
+        if time.time() - time0 > 5:
+            print("Timeout")
+            tinytuya.scan()
+            devices = get_all_devices()
+            switch = devices["switches"][0]
+            switch.set_version(3.3)
+
 
 if __name__ == "__main__":
     main()
-    
